8b8d2a8858d68bd12c5332201c22a2764f5c9730
Beriti <dvarberitioliveira@gmail.com>
2026-04-02 22:41:47 -0300

    Update quartos.py

--- quartos.py
+++ quartos.py
@@ -1,6 +1,6 @@
-#criar a classe hotel
-
 from enum import Enum
+
+""" Classe TipoQuarto"""
 
 # Definição da classe TipoQuarto
 class TipoQuarto(Enum):
@@ -9,19 +9,15 @@ class TipoQuarto(Enum):
   DELUXE = "Deluxe"
   SUITE= "Suite"
 
-# Dicionário de precos
-tabela_precos = {
-    "Standard" : 100,
-    "Master" : 200,
-    "Deluxe": 300,
-    "Suite" : 500
-}
+""" Classe StatusQuarto"""
 
 # Definição da classe StatusQuarto
 class StatusQuarto(Enum):
   RESERVADO = "Reservado"
   OCUPADO = "Ocupado"
   DISPONIVEL = "Disponível"
+
+""" Classe Quarto """
 
 # Definição da classe Quarto
 class Quarto:
@@ -30,15 +26,11 @@ class Quarto:
     self.tipo = tipo
     self.preco = valor_diaria
     self.status = status
-
-  # Métodos de instância
-
-  # Pesquisa e analise a existência do quarto
-  
-  # Retorna as infomações do quarto requerido
+  # Exibe as infomações do quarto requerido
   def __str__(self):
+
     return f" Número: {self.numero}\n Tipo: {self.tipo.value}\n Preço: R${self.preco}\n Status: {self.status.value}"
-  
+
   # Muda o status do quarto selecionado
   def mudar_status(self, novo_status: str):
 
@@ -51,27 +43,44 @@ class Quarto:
     except ValueError: # é executado se novo_status não existir em StatusQuarto / valor inadequado
       print(f"{novo_status} não é um status válido")
 
+""" Classe Hotel """
 
-# Função para definir os quartos
-def definir_quartos():
-  quartos_standard = {i : Quarto(i, TipoQuarto.STANDARD, tabela_precos["Standard"]) for i in range(1, 501)} # Defini os quartos standart do 1 ao 500, 500 quartos
-  quartos_master = {i : Quarto(i, TipoQuarto.MASTER, tabela_precos["Master"]) for i in range(501, 1001)} # Defini os quartos master do 501 ao 1000, 500 quartos
-  quartos_deluxe = {i : Quarto(i, TipoQuarto.DELUXE, tabela_precos["Deluxe"])for i in range(1001, 1251)} # Defini os quartos deluxe do 1001 ao 1251, 250 quartos
-  quartos_suite = {i : Quarto(i, TipoQuarto.SUITE, tabela_precos["Suite"]) for i in range(1251, 1351)} # Defini os quartos suite do 1251 ao 1350, 100 quartos
+# Defini a classe hotel que abrangem as demais classes ligadas aos quartos
+class Hotel():
+  def __init__(self):
+    """ Dicionário de preços """
+     
+    # criação de um dicionário de precos tabelados
+    self.tabela_precos = {
+      "Standard" : 100,
+      "Master" : 200,
+      "Deluxe": 300,
+      "Suite" : 500
+    }
 
-  return  quartos_standard |  quartos_master |  quartos_deluxe |  quartos_suite # retorna todos os dicionários em um só dict
+    self.quartos = self.criar_quartos()
+ 
+  # Função de criação de quartos
+  def criar_quartos(self):
+    
+    # Cria os quartos Standard do 1 ao 500
+    standards = {i : Quarto(i, TipoQuarto.STANDARD, self.tabela_precos["Standard"]) for i in range(1, 501)} 
+    # Cria os quartos Master do 501 ao 1000
+    masters = {i : Quarto(i, TipoQuarto.MASTER, self.tabela_precos["Master"]) for i in range(501, 1001)}
+    # Cria os quartos Deluxe do 1001 ao 1250
+    deluxes = {i : Quarto(i, TipoQuarto.DELUXE, self.tabela_precos["Deluxe"]) for i in range(1001, 1251)}
+    # Cria os quartos Suite do 1251 ao 1351
+    suites = {i : Quarto(i, TipoQuarto.SUITE, self.tabela_precos["Suite"]) for i in range(1251, 1351)}
 
-# Função para procurar um quarto no dicionário conjunto
-def pesquisar_quartos(numero_quarto: int, dicionario_quartos: dict):
-  quarto = dicionario_quartos.get(numero_quarto) # procura a chave no dict quartos, se não haver retorna None
+    return standards | masters | deluxes | suites
 
-  if not quarto: return "Quarto não encontrado" # executando quando quarto = None
+  # Função de busca/validação de quartos
+  def buscar_quarto(self, numero_quarto: int):
+    quarto = self.quartos.get(numero_quarto)
 
-  else: return str(quarto) # executado quando a chave do quarto existe, retornado a função __str__
-
-   
-
-
-# Definição de variáveis importantes
-quartos = definir_quartos() # preenche o dict com todos os quartos
-
+    if quarto:
+      return quarto
+    else: 
+      print("ERRO!.........Quarto não encontrado")
+  
+hotel = Hotel()  
