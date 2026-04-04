dec6030d0dd86fdf0ca637d61bf425d88696cc2d
Beriti <dvarberitioliveira@gmail.com>
2026-04-04 03:31:02 -0300

    Refactor Quarto and Hotel classes with JSON support

--- quartos.py
+++ quartos.py
@@ -1,83 +1,85 @@
-from enum import Enum
+import json 
 
-class TipoQuarto(Enum):
-  """ Tipos de quarto """
-
-  STANDARD = "Standard"
-  MASTER = "Master"
-  DELUXE = "Deluxe"
-  SUITE= "Suite"
-
-class StatusQuarto(Enum):
-  """ Status de quarto """
-
-  RESERVADO = "Reservado"
-  OCUPADO = "Ocupado"
-  DISPONIVEL = "Disponível"
-
-class Quarto:
-
-  def __init__(self, numero: int, tipo: TipoQuarto, valor_diaria: float, status: StatusQuarto = StatusQuarto.DISPONIVEL):
-    """ Atributos do quarto """
-
+class Quarto():
+  def __init__(self, numero: str, tipo: str, status: str, preco: str):
     self.numero = numero
     self.tipo = tipo
-    self.preco = valor_diaria
     self.status = status
-
-  # Exibe as infomações do quarto
+    self.preco = float(preco)
+    
+    self.dicionario_status = {
+      "disponivel" : "Disponível",
+      "reservado" : "Reservado",
+      "ocupado" : "Ocupado"
+    }
+  
+  # Exibe os atributos do quarto
   def __str__(self):
-
-    return f" Número: {self.numero}\n Tipo: {self.tipo.value}\n Preço: R${self.preco}\n Status: {self.status.value}"
-
-  # Valida o status recebido
-  def validar_status(self, novo_status: str):
-    novo_status = novo_status.upper().replace("Í", "I") # Upper() torna a string maiuscula igual o enumerador da StatusQuarto e replace remove o acento
-
-    return StatusQuarto.__members__.get(novo_status) # Retorna status se existir, se não retorna None
-
-  # Muda o status do quarto selecionado
+    return (f" Numero: {self.numero}\n Tipo: {self.tipo}\n Status: {self.status}\n Preço: R${self.preco}")
+  
+  # Muda o atributo staus do quarto
   def mudar_status(self, novo_status: str):
-    validacao = self.validar_status(novo_status) # Valida o status antes de mudar (Possivel mudança para validação no codigo principal)
+    novo_status = self.dicionario_status.get(novo_status, None) # Valida o novo_status 
 
-    if validacao is None:
-      print("ERRO!.........Status não encontrado")
-
+    if not novo_status:
+      print("Status não existente")
+      
     else:
-      self.status = validacao
+      self.status = novo_status
 
-      print(f"O quarto {self.numero} agora está {self.status.value}") # Printa o valor do enumerador de StatusQuarto
+      print("Status atualizado com sucesso!")  
 
 class Hotel():
-
   def __init__(self):
-    """ Atributos de quarto """
+    self.quartos = {}
 
-    # criação de um dicionário de precos tabelados
-    self.tabela_precos = {
-      "Standard" : 100,
-      "Master" : 200,
-      "Deluxe": 300,
-      "Suite" : 500
+    self.tipos = {
+      "standard" : "Standard",
+      "master" : "Master",
+      "deluxe" : "Deluxe",
+      "suite" : "Suite"
     }
 
-    self.quartos = self.criar_quartos() # Uni os quartos em um único dict
+    self.precos = {
+      "standard" : "150",
+      "master" : "300",
+      "deluxe" : "450",
+      "suite" : "600"
+    }
+  
+  # Carrega os dados dos quartos
+  def carregar_quartos(self):
+    with open("Quartos.json", "r", encoding="utf-8") as arquivo:
 
-  # Cria um dict com todos os quartos
-  def criar_quartos(self):
+      self.dicionario = json.load(arquivo)
+      self.quartos = {item['numero']: Quarto(**item) for item in self.dicionario}
 
-    # Cria os quartos Standard do 1 ao 500
-    standards = {i : Quarto(i, TipoQuarto.STANDARD, self.tabela_precos["Standard"]) for i in range(1, 501)}
-    # Cria os quartos Master do 501 ao 1000
-    masters = {i : Quarto(i, TipoQuarto.MASTER, self.tabela_precos["Master"]) for i in range(501, 1001)}
-    # Cria os quartos Deluxe do 1001 ao 1250
-    deluxes = {i : Quarto(i, TipoQuarto.DELUXE, self.tabela_precos["Deluxe"]) for i in range(1001, 1251)}
-    # Cria os quartos Suite do 1251 ao 1351
-    suites = {i : Quarto(i, TipoQuarto.SUITE, self.tabela_precos["Suite"]) for i in range(1251, 1351)}
+      return self.quartos
+  # Salva os dados dos quartos
+  def salvar_quartos(self):
+    quartos_formatados = [{chave: valor for chave, valor in q.__dict__.items() if chave != 'dicionario_status'} for q in self.quartos.values()]
 
-    return standards | masters | deluxes | suites
 
-  # Valida o número de quarto recebido
-  def validar_quarto(self, numero_quarto: int):
-    return self.quartos.get(numero_quarto) # Retorna quarto se existir, se não retorna None
+    with open("Quartos.json", "w", encoding="utf-8") as arquivo:
+      json.dump(quartos_formatados, arquivo, indent=4, ensure_ascii=False)
+  
+  # Busca um quarto 
+  def pesquisar_quarto(self, numero_quarto: str):
+    
+    return self.quartos.get(numero_quarto, None) # Valida a busca
+
+    
+  
+raiteis = Hotel()
+
+raiteis.carregar_quartos()
+
+
+
+(raiteis.pesquisar_quarto("101")).mudar_status("disponivel")
+
+
+
+raiteis.salvar_quartos()
+
 
